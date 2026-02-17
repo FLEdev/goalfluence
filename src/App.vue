422f69e0722588b7014dae8a226fa72545773950
user <f.leginsky@gmail.com>
2026-02-17 06:12:40 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    GF
+    <div class=""></div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+</style>
