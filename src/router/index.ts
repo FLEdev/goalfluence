00d4aae88de90d62fc8c73449afa740e577438b0
user <f.leginsky@gmail.com>
2026-02-18 17:42:02 +0100

    fixed tsconfig

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,7 +16,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/About.vue')
+      component: About
     },
     {
       path: '/',
